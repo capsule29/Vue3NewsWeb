58fc1ac399249220ae8ab5f89c54b5d420dbfb3a
capsule <1729597058@qq.com>
2024-04-07 19:06:43 +0800

    更改新闻修改页面背景以及参数

--- src/pages/NewsRichEditor.vue
+++ src/pages/NewsRichEditor.vue
@@ -1,94 +1,88 @@
 <template>
-    <div class="editorWrapper">
-        <el-row>
-            <el-col :span="20" style="margin: 0px auto">
-                <el-card style="margin: 20px 0px">
-                    <!-- 正文部分 -->
-                    <el-card shadow="hover" style="text-align: center; margin: 20px auto">
-                        <!-- 卡片头 -->
-                        <template #header> 正文内容 </template>
-                        <RichEditor
-                            :model-value="editNews.news_content"
-                            @update:modelValue="handleUpdateValue"
-                        ></RichEditor>
-                    </el-card>
-                    <div class="InfoFlex">
-                        <el-card shadow="hover" style="text-align: center">
-                            <!-- 卡片头 -->
-                            <template #header> 基础信息 </template>
-                            <div>
-                                <div>
-                                    标题:
-                                    <el-tag type="info" size="large">
-                                        {{ editNews.news_title }}
-                                    </el-tag>
-                                </div>
-                                <br />
-                                <div>
-                                    作者:
-                                    <el-tag type="info" size="large">
-                                        {{ editNews.news_writer_name }}
-                                    </el-tag>
-                                </div>
-                                <br />
-                                <div>
-                                    点赞数:
-                                    <el-tag type="info" size="large">
-                                        {{ editNews.news_praise_number }}
-                                    </el-tag>
-                                </div>
-                                <br />
-                                <div>
-                                    收藏数:
-                                    <el-tag type="info" size="large">
-                                        {{ editNews.news_star_number }}
-                                    </el-tag>
-                                </div>
-                                <br />
-                                <div>
-                                    创建时间:
-                                    <el-tag type="info" size="large">
-                                        {{
-                                            new Date(
-                                                editNews.news_created_time as string
-                                            ).getFullYear()
-                                        }}
-                                        年
-                                        {{
-                                            new Date(
-                                                editNews.news_created_time as string
-                                            ).getMonth() + 1
-                                        }}
-                                        月
-                                        {{
-                                            new Date(editNews.news_created_time as string).getDate()
-                                        }}
-                                        日
-                                    </el-tag>
-                                </div>
-                            </div>
-                        </el-card>
-                        <!-- 可见部门穿梭框 -->
-                        <el-card shadow="hover" style="text-align: center">
-                            <!-- 卡片头 -->
-                            <template #header> 部门能见度 </template>
-                            <TransferTag />
-                        </el-card>
-                    </div>
-
-                    <!-- 卡片尾 -->
-                    <template #footer>
-                        <div style="text-align: center">
-                            <el-button type="success" size="large">保存</el-button>
-                            <el-button type="info" size="large" plain>返回</el-button>
-                            <!-- <el-button @click="look(1)"> 查看editorValue内容 </el-button>
-                                <el-button @click="look(2)"> 查看richHtml内容 </el-button> -->
-                        </div>
-                    </template>
+    <div class="bg"></div>
+    <el-row>
+        <el-col :span="20" style="margin: 0px auto">
+            <el-card style="margin: 20px 0px">
+                <!-- 正文部分 -->
+                <el-card shadow="hover" style="text-align: center; margin: 20px auto">
+                    <!-- 卡片头 -->
+                    <template #header> 正文内容 </template>
+                    <RichEditor
+                        :model-value="editNews.news_content"
+                        @update:modelValue="handleUpdateValue"
+                    ></RichEditor>
                 </el-card>
-            </el-col>
-        </el-row>
-    </div>
+                <div class="InfoFlex">
+                    <el-card shadow="hover" style="text-align: center">
+                        <!-- 卡片头 -->
+                        <template #header> 基础信息 </template>
+                        <div>
+                            <div>
+                                标题:
+                                <el-tag type="info" size="large">
+                                    {{ editNews.news_title }}
+                                </el-tag>
+                            </div>
+                            <br />
+                            <div>
+                                作者:
+                                <el-tag type="info" size="large">
+                                    {{ editNews.news_writer_name }}
+                                </el-tag>
+                            </div>
+                            <br />
+                            <div>
+                                点赞数:
+                                <el-tag type="info" size="large">
+                                    {{ editNews.news_praise_number }}
+                                </el-tag>
+                            </div>
+                            <br />
+                            <div>
+                                收藏数:
+                                <el-tag type="info" size="large">
+                                    {{ editNews.news_star_number }}
+                                </el-tag>
+                            </div>
+                            <br />
+                            <div>
+                                创建时间:
+                                <el-tag type="info" size="large">
+                                    {{
+                                        new Date(editNews.news_created_time as string).getFullYear()
+                                    }}
+                                    年
+                                    {{
+                                        new Date(editNews.news_created_time as string).getMonth() +
+                                        1
+                                    }}
+                                    月
+                                    {{ new Date(editNews.news_created_time as string).getDate() }}
+                                    日
+                                </el-tag>
+                            </div>
+                        </div>
+                    </el-card>
+                    <!-- 可见部门穿梭框 -->
+                    <el-card shadow="hover" style="text-align: center">
+                        <!-- 卡片头 -->
+                        <template #header> 部门能见度 </template>
+                        <TransferTag />
+                    </el-card>
+                </div>
+
+                <!-- 卡片尾 -->
+                <template #footer>
+                    <div style="text-align: center">
+                        <el-button type="success" size="large">保存</el-button>
+                        <el-button type="info" size="large" plain>返回</el-button>
+                        <!-- <el-button @click="look(1)"> 查看editorValue内容 </el-button>
+                                <el-button @click="look(2)"> 查看richHtml内容 </el-button> -->
+                    </div>
+                </template>
+            </el-card>
+        </el-col>
+    </el-row>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue'
@@ -108,8 +102,6 @@ const handleUpdateValue = (val: any) => {
 }
 
 const route = useRoute()
-let news_title_Ref = ref('')
-let news_writer_name_Ref = ref('')
 
 import { reactive } from 'vue'
 import type { News } from '@/api/news/NewsModel'
@@ -132,13 +124,7 @@ let editNews: newsNews = reactive({
 
 // 初始化数据
 const getData = () => {
-    const title: string = route.params.news_title as string
-    news_title_Ref.value = title
-
-    const name: string = route.params.news_writer_name as string
-    news_writer_name_Ref.value = name
-
-    // const content: string = route.params.news_content as string
+    
 }
 
 // 挂载完毕
@@ -158,11 +144,14 @@ onMounted(() => {
 // }
 </script>
 <style scoped>
-.editorWrapper {
-    /* margin-top: 20px; */
+.bg {
     background-image: url('@/assets/bg2.png');
     background-repeat: no-repeat;
     background-size: cover;
+
+    height: 100%;
+    width: 100%;
+    position: fixed;
 }
 
 /* 自定义布局 */
